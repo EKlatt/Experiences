a88d6920d6d55610731288acec7cf9d458712e0c
Enno Klatt <73426221+EKlatt@users.noreply.github.com>
2022-12-05 12:11:49 +0100

    Create Hilfe.ino

---
+++ WOKWI/EBW_07/Hilfe.ino
@@ -0,0 +1,6 @@
+// https://briankhuu.com/blog/self/2015/07/05/understanding-more-about-avr-programming.html
+// https://de.wikibooks.org/wiki/C-Programmierung_mit_AVR-GCC/_IO-Ports
+// https://www.instructables.com/Beginning-Arduino-Ports-Pins-and-Programming/
+// https://arbaranwal.github.io/tutorial/2017/06/23/atmega328-register-reference.html
+// https://playground.arduino.cc/Code/BitMath/
+
