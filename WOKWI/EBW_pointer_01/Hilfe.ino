fa9971c1ade01eacfc3e6dafee83bf6f447b558a
Enno Klatt <73426221+EKlatt@users.noreply.github.com>
2022-12-05 12:11:44 +0100

    Create Hilfe.ino

---
+++ WOKWI/EBW_pointer_01/Hilfe.ino
@@ -0,0 +1,6 @@
+// https://briankhuu.com/blog/self/2015/07/05/understanding-more-about-avr-programming.html
+// https://arduino.stackexchange.com/questions/56304/how-do-i-directly-access-a-memory-mapped-register-of-avr-with-c
+// https://de.wikibooks.org/wiki/C-Programmierung_mit_AVR-GCC/_IO-Ports
+// https://arbaranwal.github.io/tutorial/2017/06/23/atmega328-register-reference.html
+// https://playground.arduino.cc/Code/BitMath/
+
