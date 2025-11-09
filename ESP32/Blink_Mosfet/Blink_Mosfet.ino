4059f3ae32bb5ed4c69c953193b9366e37efe10c
Enno Klatt <73426221+EKlatt@users.noreply.github.com>
2025-11-09 12:45:02 +0100

    Create Blink_Mosfet.ino

---
+++ ESP32/Blink_Mosfet/Blink_Mosfet.ino
@@ -0,0 +1,19 @@
+// EBW: Experimentieren mit dem „ESP-32 Dev Kit C V2“
+// Sketch Blink_Mosfet.ino 
+// Control an LED with a MOSFET
+
+#define ledPin 4             // GPIO where ledPin is connected to
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital "ledPin" as an output.
+  pinMode(ledPin, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(ledPin, HIGH);  // turn the LED on (HIGH is the voltage level)
+  // delay(1000);                 // wait for a second
+  // digitalWrite(ledPin, LOW);   // turn the LED off by making the voltage LOW
+  // delay(1000);                 // wait for a second
+}
