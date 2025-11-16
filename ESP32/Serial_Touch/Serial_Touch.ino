9c1f8d03f0cae0cddc839c33c5f510ba0a6aac9a
Enno Klatt <73426221+EKlatt@users.noreply.github.com>
2025-11-16 17:57:34 +0100

    Create Serial_Touch.ino

---
+++ ESP32/Serial_Touch/Serial_Touch.ino
@@ -0,0 +1,26 @@
+// EBW: Experimentieren mit dem „ESP-32 Dev Kit C V2“
+// Sketch Serial_Touch.ino 
+// Get data from touchsensor
+
+// Important notices:
+// Choose as baudrate in Serial.begin() always 115200 Baud
+// Create a while-loop to wait for Serial-connection
+// Change the "Serial Monitor Settings" to 115200 Baud
+// COnnect a jumper-wire with GPIO 15 as touch-sensor
+
+const int touchPin = 15;     // GPIO15 defined as Touch 3
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  while(!Serial) {}          // wait for established serial connection
+  Serial.println("EBW-Workshop");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  Serial.print("Touch-Sensor: ");
+  Serial.println(touchRead(touchPin));   // predifined function touchRead()
+  delay (500);
+}
